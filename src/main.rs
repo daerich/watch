bbac5776a312423d5c93bb2bee573f74e42866ec
DaErich <daerich@sourceforge.net>
2021-07-21 17:50:44 +0200

    Use getopts library; Bump version;Minor optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use ncurses;
+use getopts;
 use std::thread;
 use std::process;
 use std::time::Duration;
 use std::env;
 use std::sync::mpsc;
+use std::ops::Add;
+use std::process::exit;
 
 fn get_shell() -> std::string::String{
     for var in env::vars(){
@@ -37,12 +40,54 @@ fn run_command(shell : &std::string::String, arg_str: &std::string::String) -> s
     res
 }
 
+fn atoi(dur : &String) -> u64{
+    match dur.parse::<u64>(){
+        Ok(res) => {
+            res
+        }
+        Err(_) =>{
+            println!("Please specify an integer");
+            exit(1);
+        }
+    }
+}
+
 fn main() -> std::result::Result<(),()> {
+
+    let mut opts = getopts::Options::new();
+    opts.parsing_style(getopts::ParsingStyle::StopAtFirstFree);
+    opts.opt("n", "interval", "Set refresh interval", "Integer", getopts::HasArg::Yes, getopts::Occur::Optional);
+    let optstring : Vec<String> = env::args().collect();
     
     if env::args().len() <= 1{
-        print!("USAGE:\n watch [COMMAND]\n");
+        let progstr = env::args().nth(0).unwrap() + " [OPTION]" + " [COMMAND]";
+        println!("{}",opts.usage(&progstr));
         return Ok(());
     }
+    /* Construct program parameters */
+    let durstr : String;
+    let mut command_str = String::new();
+
+    if let Ok(res) = opts.parse(&optstring[1..]){ /* argparse */
+        if res.opt_present("n") {
+            durstr = res.opt_str("n").unwrap();
+        }
+        else{
+            durstr = String::from("2");
+        }
+
+        let sanstr = res.free.clone(); /* Concat */
+        for frag in sanstr{
+            command_str.push_str(&frag);
+            command_str.push_str(" ");
+        }
+    }
+    else{
+        println!("Couldn't parse optstring!\nMaybe you used illegal arguments?\
+        \nOr too many of them, or multiple times,or..Computer says no?");
+        exit(1); /* Better than a ugly panic */
+    };
+    
     let mut arg_str = String::new(); /* Create prompt string */
     for arg in env::args(){
         arg_str.push_str(" ");
@@ -50,35 +95,24 @@ fn main() -> std::result::Result<(),()> {
     }
     
     let shell = get_shell();
-    /* Construct command string */
-    let mut command_str = String::new();
-    {
-        let mut count = 0;
-        for arg in env::args(){
-           
-            if count == 0{
-                count += 1;
-                continue;
-            }
-            command_str.push_str(" ");
-            command_str.push_str(&arg);
-            count += 1;
-        }
-    }
+    let duration = Duration::new(atoi(&durstr),0);
+
     /* Create tui */
     ncurses::initscr();
+    let timestr =  String::from("Every ").add(&durstr).add(" seconds:");
     /* Start output routine */
     loop{
         ncurses::clear();
         let mut strn = String::new();
-        ncurses::mvaddstr(0, 0, &arg_str);
-        ncurses::mvaddstr(1, 0, "---");
+        ncurses::mvaddstr(0, 0, &timestr);
+        ncurses::mvaddstr(1, 0, &arg_str);
+        ncurses::mvaddstr(2, 0, "---");
         let resstr = run_command(&shell, &command_str);
         strn.push_str(&resstr);
-        ncurses::mvaddstr(2, 0,&strn);
+        ncurses::mvaddstr(3, 0,&strn);
         ncurses::refresh();
-        thread::sleep(Duration::new(2, 0));
+        thread::sleep(duration);
     }
-    ncurses::endwin(); /* Unreachable but call it anyways */
+    ncurses::endwin(); /* Unreachable but call it anyways */ 
     Ok(())
 }
